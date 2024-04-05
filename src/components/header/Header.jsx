9d01a8a48e6f6c4064256b48c89747b2474129ba
saaya-code <boualimohamed74@gmail.com>
2024-04-05 06:36:07 +0100

    fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { AvatarSVG, Github, Email, Figma, Linkedin, AvatarPNG } from "../../assets/image";
+import { Github, Email, Linkedin, AvatarPNG } from "../../assets/image";
 import { FaEnvelope, FaLink, FaLocationDot, FaPhone } from "react-icons/fa6";
 
 const Header = () => {
